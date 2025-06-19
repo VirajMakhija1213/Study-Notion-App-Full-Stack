672bdec5addf154c0a05e54b3b5d419c7839f2f8
Viraj Makhija <160765399+VirajMakhija1213@users.noreply.github.com>
2025-06-20 01:50:27 +0530

    Update ResetPassword.js

--- server/controllers/ResetPassword.js
+++ server/controllers/ResetPassword.js
@@ -25,7 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
 		);
 		console.log("DETAILS", updatedDetails);
 
-		const url = `https://study-notion-viraj.vercel.app/${token}`;
+		const url = `https://study-notion-viraj.vercel.app/update-password/${token}`;
 		await mailSender(
 			email,
 			"Password Reset",
