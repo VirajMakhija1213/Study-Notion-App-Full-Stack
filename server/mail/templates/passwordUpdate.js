83752e130c98b896f4f91397580a7fb8733e1bcf
Viraj Makhija <160765399+VirajMakhija1213@users.noreply.github.com>
2025-06-21 14:58:29 +0530

    Update passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,7 +55,7 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
+            <a href="https://study-notion-viraj.vercel.app"><img class="logo"
                     src="https://www.figma.com/community/resource/ce2bc700-da65-4a28-a6a5-f6ef0b998a86/thumbnail" alt="StudyNotion Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
